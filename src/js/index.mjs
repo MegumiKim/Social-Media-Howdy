c77a2f805f47bb95d0e9f541628b3b77c280b5c1
Megumi Kimura <kimuramegumi@gmail.com>
2022-12-05 22:31:55 +0100

    add router

--- src/js/index.mjs
+++ src/js/index.mjs
@@ -1,18 +1,1 @@
-import * as listeners from "./listeners/index.mjs";
-import * as renders from "./renders/index.mjs";
-
-listeners.signUp();
-listeners.login();
-listeners.createPostListener();
-listeners.myProfileLink();
-listeners.searchPostListener();
-listeners.searchUserListener();
-listeners.oldToNewListener();
-listeners.newToOldListener();
-listeners.logout();
-
-renders.renderPosts();
-renders.renderSinglePost();
-renders.renderProfiles();
-renders.renderSingleProfile();
-renders.renderUserSpecificPosts();
+import "./router.mjs";
