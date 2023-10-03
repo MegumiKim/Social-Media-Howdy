e8b2b5f3ab9e1a24c38741815822fe8eb158e027
Megumi Kimura <kimuramegumi@gmail.com>
2023-10-03 21:33:48 +0200

    fix login

--- src/js/utils/forceLogIn.mjs
+++ src/js/utils/forceLogIn.mjs
@@ -2,6 +2,8 @@ import { load } from "../storage/local.mjs";
 
 /**Prevent user to visit the auth-restricted pages without access token */
 export function forceLogIn() {
+  console.log("canceled");
+
   // if (!load("accessToken")) {
   //   location.href = "../../../index.html";
   // }
