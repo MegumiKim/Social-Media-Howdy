d3f9fda33851690061a7677c2e89a07f45b8af9d
Megumi Kimura <kimuramegumi@gmail.com>
2023-10-04 09:08:53 +0200

    fix internal links

--- src/js/api/profiles/loginUser.mjs
+++ src/js/api/profiles/loginUser.mjs
@@ -1,9 +1,5 @@
 import { BASE_URL } from "./../constants.mjs";
-<<<<<<< HEAD
 import * as storages from "../../storage/index.mjs";
-=======
-import * as storages from "./../../storage/index.mjs";
->>>>>>> 3e24b00d5ade7e267ce4d7e3b2de39caddf8e5b9
 
 const registerURL = `${BASE_URL}/auth/login`;
 const userAlert = document.querySelector("#user-alert");
@@ -26,12 +22,7 @@ export async function loginUser(options) {
   if (accessToken) {
     storages.locals.save("accessToken", accessToken);
     storages.locals.save("otherDetails", otherDetails);
-<<<<<<< HEAD
     window.location.assign("./posts/index.html");
-=======
-    window.location.href("./posts/");
-    // window.location.assign("./posts/");
->>>>>>> 3e24b00d5ade7e267ce4d7e3b2de39caddf8e5b9
   } else {
     const { errors } = json;
     errors.forEach(({ message }) => {
